44af4ca7cefac9bb33458b1761ab56ccc335bcb4
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2022-09-04 00:59:55 +0300

    Update template.php

--- custom/templates/RadomeWEB/template.php
+++ custom/templates/RadomeWEB/template.php
@@ -49,7 +49,7 @@ class RadomeWEB_Template extends TemplateBase {
         $this->addCSSFiles([
 			'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css' => array('integrity' => 'sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2', 'crossorigin' => 'anonymous'),
 			(defined('CONFIG_PATH') ? CONFIG_PATH : '') . '/core/assets/plugins/toastr/toastr.min.css' => array('rel' => 'preload', 'as' => 'style', 'onload' => "this.onload=null;this.rel='stylesheet'"),
-			$template['path'] . 'css/radomeweb.css?v=200',
+			$template['path'] . 'css/radomeweb.css?v=' . Output::getClean($radomeweb_local_version) => array(),
 			'https://use.fontawesome.com/releases/v5.15.1/css/all.css' => array('rel' => 'preload', 'as' => 'style', 'onload' => "this.onload=null;this.rel='stylesheet'")
 		]);
 
