d918670f20e52e513060e795a0eecaabb9214eef
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2022-09-05 23:43:05 +0300

    Update index.php

--- modules/Wiki/pages/panel/index.php
+++ modules/Wiki/pages/panel/index.php
@@ -224,7 +224,7 @@ if(!isset($_GET['action'])){
 			if(Input::exists()){
 				$errors = [];
 				if(Token::check(Input::get('token'))){
-					$validate = new Validate();
+					$validate = Validate::__construct();
 					$validation = $validate->check($_POST, [
 						'wiki_page_title' => [
 							'required' => true,
