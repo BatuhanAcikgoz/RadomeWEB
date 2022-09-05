1b7eb8f2f8afa7e779ca9cc624171766d7d6f527
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2022-09-05 22:26:51 +0300

    Update core.js

--- custom/templates/RadomeWEB/js/core/core.js
+++ custom/templates/RadomeWEB/js/core/core.js
@@ -13,17 +13,17 @@ $(document).ready(function () {
     });
 });
 
-$('.navbar-toggler').click(function() {
+$('.navbar-toggler').click(function () {
     $('.coldfire-navbar-menu').addClass("active");
     $('.overlay').addClass("active");
 });
-$('#nav-header-close').click(function() {
+$('#nav-header-close').click(function () {
     $('.coldfire-navbar-menu').removeClass("active");
     $('.overlay').removeClass("active");
 });
 
 if ((particles == "yes") && ($("#header-pjs").length)) {
-    $(window).on("load", function() {
+    $(window).on("load", function () {
         particlesJS.load("header-pjs", pjsPath);
     });
 }
@@ -32,15 +32,15 @@ if (loadingTime) {
     $('#page_load_tooltip').attr('title', loadingTime).tooltip();
 }
 
-$(document).ready(function() {
-    $(window).scroll(function() {
+$(document).ready(function () {
+    $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
             $("#scroll").stop().fadeIn();
         } else {
             $("#scroll").stop().fadeOut();
         }
     });
-    $("#scroll").click(function() {
+    $("#scroll").click(function () {
         $("html, body").animate({ scrollTop: 0 }, 600);
         return false;
     });
@@ -50,15 +50,15 @@ $.fn.tooltip.Constructor.Default.whiteList["span"].push("style");
 $.fn.tooltip.Constructor.Default.whiteList["a"].push("style");
 
 $(".pop").popover({ trigger: "manual", html: "true", placement: "top" })
-    .on("mouseenter", function() {
+    .on("mouseenter", function () {
         var _this = this;
         $(this).popover("show");
-        $(".popover").on("mouseleave", function() {
+        $(".popover").on("mouseleave", function () {
             $(_this).popover('hide');
         });
-    }).on("mouseleave", function() {
+    }).on("mouseleave", function () {
         var _this = this;
-        setTimeout(function() {
+        setTimeout(function () {
             if (!$(".popover:hover").length) {
                 $(_this).popover('hide');
             }
@@ -66,38 +66,38 @@ $(".pop").popover({ trigger: "manual", html: "true", placement: "top" })
     });
 
 $('.more-dropdown').hover(
-    function() {
+    function () {
         $(this).find('.dropdown-menu').stop(true, true).delay(25).fadeIn();
     },
-    function() {
+    function () {
         $(this).find('.dropdown-menu').stop(true, true).delay(25).fadeOut();
     }
 );
 $('.more-dropdown-menu').hover(
-    function() {
+    function () {
         $(this).stop(true, true);
     },
-    function() {
+    function () {
         $(this).stop(true, true).delay(25).fadeOut();
     }
 );
 
-$(function() {
+$(function () {
     $('[data-toggle="tooltip"]').tooltip()
 });
-$(function() {
+$(function () {
     $('[rel="tooltip"]').tooltip()
 });
 
-$('[data-toggle="popover"]').popover({ trigger: "manual", html: true, animation: false }).on("mouseenter", function() {
+$('[data-toggle="popover"]').popover({ trigger: "manual", html: true, animation: false }).on("mouseenter", function () {
     var _this = this;
     $(this).popover("show");
-    $(".popover").on("mouseleave", function() {
+    $(".popover").on("mouseleave", function () {
         $(_this).popover('hide');
     });
-}).on("mouseleave", function() {
+}).on("mouseleave", function () {
     var _this = this;
-    setTimeout(function() {
+    setTimeout(function () {
         if (!$(".popover:hover").length) {
             $(_this).popover("hide");
         }
@@ -113,20 +113,12 @@ $(document).ready(function () {
             timeoutId = window.setTimeout(function () {
                 timeoutId = null;
                 if (!($(elem).data('poload') in cachedUsers)) {
-                    $(elem).popover({
-                        trigger: "manual",
-                        animation: false,
-                        content: loadingTime
-                    }).popover("show");
+                    $(elem).popover({ trigger: "manual", animation: false, content: "Yükleniyor..." }).popover("show");
                     $.get($(elem).data('poload'), function (d) {
                         (debugging && debugging == '1' ? console.log(d) : '');
-                        var data = JSON.parse(d);
+                        const data = JSON.parse(d);
                         cachedUsers[$(elem).data('poload')] = data;
-                        $(elem).popover("dispose").popover({
-                            trigger: "manual",
-                            animation: false,
-                            content: data.html
-                        }).popover("show");
+                        $(elem).popover("dispose").popover({ trigger: "manual", animation: false, content: data.html }).popover("show");
                         $('.popover').mouseleave(function () {
                             if (!$(".popover:hover").length) {
                                 $(this).popover("hide");
@@ -134,12 +126,8 @@ $(document).ready(function () {
                         });
                     });
                 } else {
-                    var data = cachedUsers[$(elem).data('poload')];
-                    $(elem).popover({
-                        trigger: "manual",
-                        animation: false,
-                        content: data.html
-                    }).popover("show");
+                    const data = cachedUsers[$(elem).data('poload')];
+                    $(elem).popover({ trigger: "manual", animation: false, content: data.html }).popover("show");
                     $('.popover').mouseleave(function () {
                         if (!$(".popover:hover").length) {
                             $(this).popover("hide");
@@ -162,11 +150,12 @@ $(document).ready(function () {
         }
     });
 
-    var timezone = document.getElementById('timezone');
+    const timezone = document.getElementById('timezone');
+
     if (timezone) {
         const timezoneValue = Intl.DateTimeFormat().resolvedOptions().timeZone;
         if (timezoneValue) {
-          timezone.value = timezoneValue;
+            timezone.value = timezoneValue;
         }
     }
 });
@@ -179,7 +168,7 @@ function copyToClipboard(element) {
     $temp.remove();
 
     Swal.fire({
-        title: "IP " + copied, 
+        title: "IP " + copied,
         text: swal_server_copy,
         icon: "success",
         confirmButtonText: close
