583d7a1429cb20c2eed40e81907ccf5dc88e02eb
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2022-09-07 13:18:12 +0300

    Update module.php

--- modules/Wiki/module.php
+++ modules/Wiki/module.php
@@ -183,7 +183,7 @@ class Wiki_Module extends Module {
 			$charset = 'latin1';
 
 		try {
-			$group = $this->_db->get('groups', ['id', '=', 1]);
+			$group = $this->_db->get('groups', array('id', '=', 2))->results();
 			$group = $group[0];
 			
 			$group_permissions = json_decode($group->permissions, TRUE);
