27da865005c4afe418fe7feb46dcb6a5262006d1
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2022-09-28 23:47:11 +0300

    Create 20220928121054_create_forms_table.php

---
+++ core/migrations/20220928121054_create_forms_table.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class CreateFormsTable extends AbstractMigration
+{
+    public function change(): void
+    {
+        $table = $this->table('nl2_forms');
+
+        $table
+            ->addColumn('id', 'integer', ['length' => 11])
+            ->addColumn('url', 'string', ['length' => 32])
+            ->addColumn('title', 'string', ['length' => 32])
+            ->addColumn('guest', 'smallinteger', ['length' => 1, 'null' => false, 'default' => 0])
+            ->addColumn('link_location', 'smallinteger', ['length' => 1, 'null' => false, 'default' => 1])
+            ->addColumn('icon', 'string', ['length' => 64])
+            ->addColumn('can_view', 'smallinteger', ['length' => 1, 'null' => false, 'default' => 0])
+            ->addColumn('captcha', 'smallinteger', ['length' => 1, 'null' => false, 'default' => 0])
+            ->addColumn('content', 'text', ['null' => true, 'default' => null])
+            ->addColumn('comment_status', 'integer', ['length' => 11, 'null' => false, 'default' => 0])
+            ->addColumn('source', 'string', ['length' => 32, 'null' => false, 'default' => 'forum'])
+            ->addColumn('forum_id', 'integer', ['length' => 11, 'null' => false, 'default' => 0]);
+
+        $table->create();
+    }
+}
