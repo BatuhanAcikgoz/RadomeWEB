92af036643f704f38a2c2e96a6025d31e94e9f1d
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2022-09-29 19:42:48 +0300

    bugfix

--- core/migrations/20220928001323_create_forms_replies_fields_table.php
+++ core/migrations/20220928001323_create_forms_replies_fields_table.php
@@ -14,8 +14,6 @@ final class CreateFormsRepliesFieldsTable extends AbstractMigration
             ->addColumn('field_id', 'integer', ['length' => 11])
             ->addColumn('value', 'text', ['null' => false]);
 
-        $table = $this->query('ALTER TABLE `nl2_forms_replies_fields` ADD INDEX `nl2_forms_replies_fields_idx_submission_id` (`submission_id`)');
-
         $table->create();
     }
 }
