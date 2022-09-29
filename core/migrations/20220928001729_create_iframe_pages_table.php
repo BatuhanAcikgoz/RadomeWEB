d3e0160e09851c0873dddbd44a74b9f713397c9d
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2022-09-29 19:35:40 +0300

    Update 20220928001729_create_iframe_pages_table.php

--- core/migrations/20220928001729_create_iframe_pages_table.php
+++ core/migrations/20220928001729_create_iframe_pages_table.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 use Phinx\Migration\AbstractMigration;
 
-final class CreateIframeTable extends AbstractMigration
+final class CreateIframePagesTable extends AbstractMigration
 {
     public function change(): void
     {
