d4a86bae6ae3b9353045030187c72273d433e6ee
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2022-10-03 11:48:22 +0300

    Update DatabaseInitialiser.php

--- core/classes/Database/DatabaseInitialiser.php
+++ core/classes/Database/DatabaseInitialiser.php
@@ -701,32 +701,8 @@ class DatabaseInitialiser {
                 'zort'
             ),
             'post_date' => date('Y-m-d H:i:s'),
-            'created' => date('U'),
-            'last_edited' => date('U'),
+            'created' => date('U')
         ]);
-
-        // Must be updated afterwards due of foreign key
-        $this->_db->update('forums', 2, [
-            'last_post_date' => date('U'),
-            'last_user_posted' => 1,
-            'last_topic_posted' => 1,
-        ]);
-
-        // Permissions
-        for ($i = 0; $i < 4; $i++) {
-            for ($n = 1; $n < 3; $n++) {
-                $this->_db->insert('forums_permissions', [
-                    'group_id' => $i,
-                    'forum_id' => $n,
-                    'view' => true,
-                    'create_topic' => ($i == 0 ? 0 : 1),
-                    'edit_topic' => ($i == 0 ? 0 : 1),
-                    'create_post' => ($i == 0 ? 0 : 1),
-                    'view_other_topics' => true,
-                    'moderate' => (($i == 2 || $i == 3) ? 1 : 0)
-                ]);
-            }
-        }
 
         // Forum Labels
         $this->_db->insert('forums_labels', [
