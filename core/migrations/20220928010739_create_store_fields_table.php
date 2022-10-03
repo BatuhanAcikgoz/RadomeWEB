adff47448e198b288395698b20cda2c762e2eb2f
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2022-10-03 12:16:05 +0300

    Update 20220928010739_create_store_fields_table.php

--- core/migrations/20220928010739_create_store_fields_table.php
+++ core/migrations/20220928010739_create_store_fields_table.php
@@ -17,7 +17,7 @@ final class CreateStoreFieldsTable extends AbstractMigration
             ->addColumn('min', 'integer', ['length' => 1, 'null' => false, 'default' => 0])
             ->addColumn('max', 'integer', ['length' => 64, 'null' => false, 'default' => 0])
             ->addColumn('options', 'text', ['null' => true])
-            ->addColumn('regex', 'string', ['length' => 64, 'null' => false, 'default' => NULL])
+            ->addColumn('regex', 'string', ['length' => 64, 'null' => true, 'default' => NULL])
             ->addColumn('default_value', 'string', ['length' => 64, 'null' => false, 'default' => ''])
             ->addColumn('deleted', 'integer', ['length' => 11, 'null' => false, 'default' => 0])
             ->addColumn('order', 'integer', ['length' => 11, 'null' => false, 'default' => 1]);
