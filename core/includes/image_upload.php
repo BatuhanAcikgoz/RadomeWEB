8f846c7c7f7b0222150b9fa81c41bddf0a6cab12
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2022-10-04 19:38:00 +0300

    Revert "Update image_upload.php"
    This reverts commit 9ae19f9e77991db8675d8b6934141e0795aae950.

--- core/includes/image_upload.php
+++ core/includes/image_upload.php
@@ -51,7 +51,7 @@ if ($_POST['type'] === 'favicon') {
 }
 
 $image = (new \Bulletproof\Image($_FILES))
-        ->setSize(1, 2097152 /* 2MB */)
+        ->setSize(1, 1048576 /* 2MB */)
         ->setDimension(2000, 2000) // 2k x 2k pixel maximum
         ->setMime($image_extensions);
 
@@ -143,7 +143,7 @@ if ($image['file']) {
 
         if (Input::get('type') === 'profile_banner') {
             $user->update([
-                'banner' => Output::getClean($user->data()->id . '/' . $image->getName() . '.' . $image->getMime())
+                'banner' => Output::getClean($user->data()->id)
             ]);
 
             Redirect::to(URL::build('/profile/' . urlencode($user->data()->username)));
