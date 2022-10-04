6e7f7aeada759eec0dab99515d894d5390cd009c
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2022-10-04 11:32:00 +0300

    UUID Linking removed

--- modules/Core/pages/panel/minecraft.php
+++ modules/Core/pages/panel/minecraft.php
@@ -73,24 +73,10 @@ $smarty->assign([
 
 if ($minecraft_enabled == 1) {
 
-    if ($user->hasPermission('admincp.minecraft.verification')) {
-        $smarty->assign([
-            'ACCOUNT_VERIFICATION' => $language->get('admin', 'account_verification'),
-            'ACCOUNT_VERIFICATION_LINK' => URL::build('/panel/minecraft/account_verification')
-        ]);
-    }
-
     if ($user->hasPermission('admincp.minecraft.servers')) {
         $smarty->assign([
             'SERVERS' => $language->get('admin', 'minecraft_servers'),
             'SERVERS_LINK' => URL::build('/panel/minecraft/servers')
-        ]);
-    }
-
-    if ($user->hasPermission('admincp.minecraft.query_errors')) {
-        $smarty->assign([
-            'QUERY_ERRORS' => $language->get('admin', 'query_errors'),
-            'QUERY_ERRORS_LINK' => URL::build('/panel/minecraft/query_errors')
         ]);
     }
 
