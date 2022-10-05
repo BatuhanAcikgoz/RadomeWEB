1c019d022a7c2b94fe8282c4f61eec0f385b6f87
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2022-10-05 09:44:59 +0300

    Update profile.php

--- modules/Core/pages/profile.php
+++ modules/Core/pages/profile.php
@@ -63,16 +63,16 @@ if (count($profile) >= 3 && ($profile[count($profile) - 1] != 'profile' || $prof
                     if ($user->data()->username == $profile) {
                         if (Token::check()) {
                             // Update banner
-                            if (isset('custom_banner')) {
+                            if (isset($_POST['banner'])) {
                                 // Check image specified actually exists
-                                if (is_file(implode(DIRECTORY_SEPARATOR, [ROOT_PATH, 'uploads', 'profile_images', 'custom_banner']))) {
+                                if (is_file(implode(DIRECTORY_SEPARATOR, [ROOT_PATH, 'uploads', 'profile_images', 'banner']))) {
                                     // Exists
                                     // Is it an image file?
-                                    if (in_array(pathinfo(implode(DIRECTORY_SEPARATOR, [ROOT_PATH, 'uploads', 'profile_images', 'custom_banner']), PATHINFO_EXTENSION), ['gif', 'png', 'jpg', 'jpeg'])) {
+                                    if (in_array(pathinfo(implode(DIRECTORY_SEPARATOR, [ROOT_PATH, 'uploads', 'profile_images', 'banner']), PATHINFO_EXTENSION), ['gif', 'png', 'jpg', 'jpeg'])) {
                                         // Yes, update settings
                                         $user->update(
                                             [
-                                                'banner' => Output::getClean('custom_banner')
+                                                'banner' => Output::getClean('banner')
                                             ]
                                         );
 
