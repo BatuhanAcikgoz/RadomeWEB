0aceb54b0d0a4466351e3bd6da6611e6bbf7b814
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2022-10-05 17:30:42 +0300

    Update user.php

--- modules/Core/pages/panel/user.php
+++ modules/Core/pages/panel/user.php
@@ -45,39 +45,22 @@ require_once(ROOT_PATH . '/core/templates/backend_init.php');
 // Load modules + template
 Module::loadPage($user, $pages, $cache, $smarty, [$navigation, $cc_nav, $staffcp_nav], $widgets, $template);
 
-if (Input::exists()) {
-    $errors = [];
+if ($user != null && $user->exists()) {
+    // Load customer by RadomeWEB User
+    if (!$this->find($user->data()->id, 'user_id')) {
+        // Customer data for RadomeWEB User missing, Register it
+        $this->create(['user_id' => $user->data()->id, 'integration_id' => 0]);
+    }
 
-    if (Token::check()) {
-        // Validation
-        $validation = Validate::check($_POST, [
-            'credits' => [
-                Validate::REQUIRED => true,
-                Validate::MIN => 1,
-                Validate::MAX => 11,
-                Validate::NUMERIC => true
-            ]
-        ]);
+    $this->_user = $user;
+} else if ($value != null) {
+    $this->find($value, $field);
 
-        if ($validation->passed()) {
-            $credits = Input::get('credits');
+} else if (Session::exists('store_customer')) {
+    $customer = Session::get('store_customer');
 
-            if (Input::get('action') == 'addCredits') {
-                $customer->addCents(Store::toCents($credits));
-
-                Session::flash('users_store_success', $store_language->get('admin', 'successfully_added_credits', ['amount' => $credits]));
-            } else if (Input::get('action') == 'removeCredits') {
-                $customer->removeCents(Store::toCents($credits));
-
-                Session::flash('users_store_success', $store_language->get('admin', 'successfully_removed_credits', ['amount' => $credits]));
-            }
-
-            Redirect::to(URL::build('/panel/users/store/', 'user=' . $view_user->data()->id));
-        } else {
-            $errors = $validation->errors();
-        }
-    } else {
-        $errors[] = $language->get('general', 'invalid_token');
+    if ($this->find($customer, 'id')) {
+        $this->_isLoggedIn = true;
     }
 }
 
