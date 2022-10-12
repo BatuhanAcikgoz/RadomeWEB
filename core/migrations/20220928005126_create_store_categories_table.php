cfe67b97a4d4d852fc48276c74de91903ebea5a9
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2022-10-12 16:04:06 +0300

    Update 20220928005126_create_store_categories_table.php

--- core/migrations/20220928005126_create_store_categories_table.php
+++ core/migrations/20220928005126_create_store_categories_table.php
@@ -14,7 +14,7 @@ final class CreateStoreCategoriesTable extends AbstractMigration
             ->addColumn('description', 'text', ['null' => false])
             ->addColumn('image', 'string', ['length' => 128, 'null' => true, 'default' => NULL])
             ->addColumn('only_subcategories', 'smallinteger', ['length' => 1, 'default' => 0])
-            ->addColumn('parent_category', 'smallinteger', ['length' => 1, 'default' => 0])
+            ->addColumn('parent_category', 'smallinteger', ['length' => 1, 'null' => true, 'default' => NULL])
             ->addColumn('hidden', 'smallinteger', ['length' => 1, 'default' => 0])
             ->addColumn('disabled', 'smallinteger', ['length' => 1, 'default' => 0])
             ->addColumn('order', 'integer', ['length' => 11])
