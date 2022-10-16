8ae0d6aa116dfa83bacab56f20791e4be80cc9c4
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2022-10-16 17:41:05 +0300

    Update products.php

--- modules/Store/pages/panel/products.php
+++ modules/Store/pages/panel/products.php
@@ -15,10 +15,6 @@ if (!$user->handlePanelPageLoad('staffcp.store.products')) {
     die();
 }
 
-if (!isset($_GET['product']) || !is_numeric($_GET['product'])) {
-    Redirect::to(URL::build('/panel/store/products'));
-}
-
 define('PAGE', 'panel');
 define('PARENT_PAGE', 'store');
 define('PANEL_PAGE', 'store_products');
