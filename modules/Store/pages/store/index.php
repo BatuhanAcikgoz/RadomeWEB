46bc442dcfd7106d94faae525b8cbc4227c2ede9
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2022-10-16 22:04:05 +0300

    Update index.php

--- modules/Store/pages/store/index.php
+++ modules/Store/pages/store/index.php
@@ -18,12 +18,20 @@ require_once(ROOT_PATH . '/modules/Store/core/frontend_init.php');
 $content = DB::getInstance()->get('store_settings', ['name', '=', 'store_content'])->results();
 $content = Output::getDecoded($content[0]->value);
 $content = Output::getPurified($content);
+$categories_list = [];
+$categories = DB::getInstance()->query('SELECT id, name FROM rw_store_categories WHERE deleted = 0')->results();
+foreach ($categories as $category) {
+    $categories_list[] = [
+        'id' => Output::getClean($category->id),
+        'name' => Output::getClean($category->name),
+    ];
+}
 
 $smarty->assign([
     'STORE' => $store_language->get('general', 'store'),
     'STORE_URL' => URL::build($store->getStoreURL()),
     'CATEGORIES' => $store->getNavbarMenu('Home'),
-    'CATEGORY_IMAGE_VALUE' => Output::getClean($category->image),
+    'CATEGORY_IMAGE_VALUE' => Output::getClean($catgory->image),
     'CONTENT' => $content,
     'TOKEN' => Token::get(),
 ]);
