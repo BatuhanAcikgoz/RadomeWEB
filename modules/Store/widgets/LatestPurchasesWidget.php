f2f2439e04c1fbe4ca9db5d7c4a49625fd978f01
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2022-10-18 00:51:19 +0300

    Update LatestPurchasesWidget.php

--- modules/Store/widgets/LatestPurchasesWidget.php
+++ modules/Store/widgets/LatestPurchasesWidget.php
@@ -49,8 +49,8 @@ class LatestStorePurchasesWidget extends WidgetBase {
             $latest_purchases_query = DB::getInstance()->query('SELECT rw_store_payments.*, identifier, username, order_id, rw_store_orders.user_id, to_customer_id FROM rw_store_payments LEFT JOIN rw_store_orders ON order_id=rw_store_orders.id LEFT JOIN rw_store_customers ON to_customer_id=rw_store_customers.id ORDER BY created DESC LIMIT ' . $purchase_limit)->results();
 			$latest_purchases = [];
 
-			$product_id  = DB::getInstance()->query('SELECT product_id FROM rw_store_orders_products')->results();
-			$product_name= DB::getInstance()->query('SELECT name FROM rw_store_products WHERE id $product_id ')->results();			
+			$product_id  = DB::getInstance()->query("SELECT 'product_id' FROM 'rw_store_orders_products'")->results();
+			$product_name= DB::getInstance()->query("SELECT 'name' FROM 'rw_store_products' WHERE id '".$product_id"'")->results();		
 
 			if (count($latest_purchases_query)) {
 				$timeago = new TimeAgo(TIMEZONE);
