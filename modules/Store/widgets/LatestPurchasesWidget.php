45abc4dac5f810cef528c3d4a5fa01689c6fed73
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2022-10-17 11:21:44 +0300

    Update LatestPurchasesWidget.php

--- modules/Store/widgets/LatestPurchasesWidget.php
+++ modules/Store/widgets/LatestPurchasesWidget.php
@@ -46,8 +46,7 @@ class LatestStorePurchasesWidget extends WidgetBase {
 				$purchase_limit = 10;
 			}
 
-
-            $latest_purchases_query = DB::getInstance()->query('SELECT rw_store_payments.*, identifier, username, order_id, name, rw_store_orders.user_id, rw_store_products.name, to_customer_id FROM rw_store_payments LEFT JOIN rw_store_orders ON order_id=rw_store_orders.id LEFT JOIN rw_store_products ON name=rw_store_products.name LEFT JOIN rw_store_customers ON to_customer_id=rw_store_customers.id ORDER BY created DESC LIMIT ' . $purchase_limit)->results();
+            $latest_purchases_query = DB::getInstance()->query('SELECT rw_store_payments.*, identifier, username, order_id, rw_store_orders.user_id, to_customer_id FROM rw_store_payments LEFT JOIN rw_store_orders ON order_id=rw_store_orders.id LEFT JOIN rw_store_customers ON to_customer_id=rw_store_customers.id ORDER BY created DESC LIMIT ' . $purchase_limit)->results();
 			$latest_purchases = [];
 
 			if (count($latest_purchases_query)) {
@@ -81,7 +80,7 @@ class LatestStorePurchasesWidget extends WidgetBase {
 						'profile' => URL::build('/profile/' . $username),
 						'price' => Output::getClean($purchase->amount),
 						'currency' => Output::getClean($purchase->currency),
-						'product' => Output::getClean($purchase->name),
+						'name' => Output::getClean($purchase->name),
 						'currency_symbol' => Output::getClean(Store::getCurrencySymbol()),
 						'uuid' => Output::getClean($purchase->identifier),
 						'date_full' => date(DATE_FORMAT, $purchase->created),
