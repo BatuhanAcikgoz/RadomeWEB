89331e303936b89a628d41af25a6b1444fbf0ef0
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2022-10-18 21:27:28 +0300

    Update LatestPurchasesWidget.php

--- modules/Store/widgets/LatestPurchasesWidget.php
+++ modules/Store/widgets/LatestPurchasesWidget.php
@@ -48,16 +48,15 @@ class LatestStorePurchasesWidget extends WidgetBase {
 
             $latest_purchases_query = DB::getInstance()->query('SELECT rw_store_payments.*, identifier, username, order_id, rw_store_orders.user_id, to_customer_id FROM rw_store_payments LEFT JOIN rw_store_orders ON order_id=rw_store_orders.id LEFT JOIN rw_store_customers ON to_customer_id=rw_store_customers.id ORDER BY created DESC LIMIT ' . $purchase_limit)->results();
 			$latest_purchases = [];
-
+			$product_id  = DB::getInstance()->query("SELECT product_id FROM rw_store_orders_products ")->results();
+			$abc= DB::getInstance()->query("SELECT name FROM rw_store_products WHERE id ='".$product_id."' ")->results();
+			$product_name= json_encode($abc, JSON_UNESCAPED_UNICODE);
 			
 			if (count($latest_purchases_query)) {
 				$timeago = new TimeAgo(TIMEZONE);
 
 				foreach ($latest_purchases_query as $purchase) {
                     // Recipient	
-					$product_id  = DB::getInstance()->query("SELECT product_id FROM rw_store_orders_products ")->results();
-					$abc= DB::getInstance()->query("SELECT name FROM rw_store_products WHERE id ='".$product_id."' ")->results();
-					$product_name= json_encode($abc, JSON_UNESCAPED_UNICODE);
                     if ($purchase->to_customer_id) {
                         $recipient = new Customer(null, $purchase->to_customer_id, 'id');
                     } else {
