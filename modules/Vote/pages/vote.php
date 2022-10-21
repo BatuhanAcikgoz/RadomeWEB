7be2ff5001496d1908c98f5a7236b0fb75952e0b
Diafwl <tekkit258@gmail.com>
2022-10-21 13:01:54 +0300

    updated vote.php

--- modules/Vote/pages/vote.php
+++ modules/Vote/pages/vote.php
@@ -43,7 +43,7 @@ $smarty->assign([
 	'MESSAGE_ENABLED' => $message_enabled,
 	'MESSAGE' => Output::getClean($vote_message),
 	'SITES' => $sites_array,
-	'MURAT' => $vote_minecraftmp
+	'MURAT' => $votes,
 ]);
 
 // Load modules + template
