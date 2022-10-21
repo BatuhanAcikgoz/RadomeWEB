0a5eb91e6775dcd789bb48d2566700fe9ee844af
Diafwl <tekkit258@gmail.com>
2022-10-21 12:55:42 +0300

    vote

--- modules/Vote/pages/vote.php
+++ modules/Vote/pages/vote.php
@@ -21,9 +21,13 @@ $vote_message = $vote_message[0]->value;
 if (!empty($vote_message)) {
 	$message_enabled = true;
 }
+$minecraftmp ="https://minecraft-mp.com/api/?object=servers&element=voters&key=rWYd7YHBqFyu6ZRfFrAMvoJBGLeIBIHBGhS&month=current&format=json&limit=5"
+$vote_minecraftmp = file_get_contents($minecraftmp)
+
 
 // Get sites from database
 $sites = DB::getInstance()->get("vote_sites", ["id", "<>", 0])->results();
+$votes = $vote_minecraftmp
 
 $sites_array = [];
 foreach ($sites as $site) {
@@ -39,6 +43,7 @@ $smarty->assign([
 	'MESSAGE_ENABLED' => $message_enabled,
 	'MESSAGE' => Output::getClean($vote_message),
 	'SITES' => $sites_array,
+	'MURAT' => $vote_minecraftmp
 ]);
 
 // Load modules + template
