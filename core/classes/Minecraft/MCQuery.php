0ec6297ca2f5d1a25443e956ee6cc247227c8322
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2022-10-25 10:22:59 +0300

    Update MCQuery.php

--- core/classes/Minecraft/MCQuery.php
+++ core/classes/Minecraft/MCQuery.php
@@ -194,7 +194,7 @@ class MCQuery {
             $formatted[] = [
                 'username' => Output::getClean($player['name']),
                 'uuid' => Output::getClean($player['name']),
-                'avatar' => (DEFAULT_AVATAR_SOURCE . DEFAULT_AVATAR_PERSPECTIVE .Output::getClean($player['name']) .'/128.png'),
+                'avatar' => (AvatarSource::getActiveSource() . Output::getClean($player['name']) .'/128.png'),
                 'profile' => URL::build('/profile/' . Output::getClean($player['name']))
             ];
         }
