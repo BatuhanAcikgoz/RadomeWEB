fab1dc7f7fcc463a0b543110ce791f100f2ea565
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2022-10-29 16:37:27 +0300

    Update users_punishments.php

--- modules/Core/pages/panel/users_punishments.php
+++ modules/Core/pages/panel/users_punishments.php
@@ -175,6 +175,7 @@ if (isset($_GET['user'])) {
                                         }
 
                                         // Fire userBanned event
+                                        $default_language = new Language('core', DEFAULT_LANGUAGE);
                                         EventHandler::executeEvent('userBanned', [
                                             'punished_id' => $query->id,
                                             'punisher_id' => $user->data()->id,
