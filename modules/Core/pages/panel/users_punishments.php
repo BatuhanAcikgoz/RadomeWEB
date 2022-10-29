6e7a670d183583364fd3c9e561424cfb652530b8
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2022-10-29 17:35:02 +0300

    Revert "Update users_punishments.php"
    This reverts commit a5a44f81de986ed99d1843ff8e3f879b34566a1c.

--- modules/Core/pages/panel/users_punishments.php
+++ modules/Core/pages/panel/users_punishments.php
@@ -189,8 +189,8 @@ if (isset($_GET['user'])) {
                                         EventHandler::executeEvent('userWarned', [
                                             'punished_id' => $query->id,
                                             'punisher_id' => $user->data()->id,
-                                            'reason' => $_POST['reason'],
                                             'footer' => $language->get('general', 'radomeweb'),
+                                            'reason' => $_POST['reason'],
                                         ]);
                                         break;
                                     case 4:
