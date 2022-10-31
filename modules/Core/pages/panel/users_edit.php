02a42cf5b1da967c630f86dc698c8f5592e4cb4d
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2022-10-31 13:57:27 +0300

    Admin şifre değiştirme özelliği

--- modules/Core/pages/panel/users_edit.php
+++ modules/Core/pages/panel/users_edit.php
@@ -186,19 +186,56 @@ if (Input::exists()) {
                     $errors[] = $language->get('admin', 'select_user_group');
                 }
             }
-        } else {
-            if (Input::get('action') == 'delete') {
-                if ($user_query->id > 1) {
-                    EventHandler::executeEvent('deleteUser', [
-                        'user_id' => $user_query->id,
-                        'username' => $user_query->username,
-                        'email_address' => $user_query->email
-                    ]);
+        } else if ((Input::get('action') == 'delete') && $user_query->id > 1) {
+            EventHandler::executeEvent('deleteUser', [
+                'user_id' => $user_query->id,
+                'username' => $user_query->username,
+                'email_address' => $user_query->email
+            ]);
 
-                    Session::flash('users_session', $language->get('admin', 'user_deleted'));
-                }
+            Session::flash('users_session', $language->get('admin', 'user_deleted'));
+            Redirect::to(URL::build('/panel/kullanicilar'));
 
-                Redirect::to(URL::build('/panel/kullanicilar'));
+        } else if ((Input::get('action') == 'change_password') && $user_query->id > 1 && !$view_user->canViewStaffCP()) {
+            $validation = Validate::check($_POST, [
+                'password' => [
+                    Validate::REQUIRED => true,
+                    Validate::MIN => 6
+                ],
+                'password_again' => [
+                    Validate::MATCHES => 'password'
+                ]
+            ])->messages([
+                'password' => [
+                    Validate::REQUIRED => $language->get('user', 'password_required'),
+                    Validate::MIN => $language->get('user', 'password_minimum_6')
+                ],
+                'password_again' => $language->get('user', 'passwords_dont_match')
+            ]);
+
+            if ($validation->passed()) {
+                $password = Input::get('password');
+                function generateSalt($length) {
+                            $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+                            $randomString = '';
+                            for ($i = 0; $i < $length; $i++) {
+                                $randomString .= $characters[rand(0, strlen($characters) - 1)];
+                            }
+                            return $randomString;
+                        }
+                        function createSHA256($password){
+                            $salt = generateSalt(16);
+                            $hash = '$SHA$'.$salt.'$'.hash('sha256', hash('sha256', $password).$salt);
+                            return $hash;
+                        }
+                        $password = createSHA256(Input::get('password'));
+                $view_user->update([
+                    'password' => $password
+                ]);
+                Session::flash('edit_user_success', $language->get('admin', 'user_password_changed_successfully'));
+            } else {
+                Session::flash('edit_user_error', implode('\n', $validation->errors()));
+
             }
         }
     } else {
@@ -254,7 +291,10 @@ if ($user_query->id != 1 && !$view_user->canViewStaffCP()) {
         'ARE_YOU_SURE' => $language->get('general', 'are_you_sure'),
         'CONFIRM_DELETE_USER' => $language->get('admin', 'confirm_user_deletion', ['user' => Output::getClean($user_query->username)]),
         'YES' => $language->get('general', 'yes'),
-        'NO' => $language->get('general', 'no')
+        'NO' => $language->get('general', 'no'),
+        'NEW_PASSWORD' => $language->get('user', 'new_password'),
+        'CONFIRM_NEW_PASSWORD' => $language->get('user', 'confirm_new_password'),
+        'CHANGE_PASSWORD' => $language->get('user', 'change_password'),
     ]);
 }
 
@@ -335,7 +375,7 @@ $smarty->assign([
     'INFO' => $language->get('general', 'info'),
     'ACTIVE_TEMPLATE' => $language->get('user', 'active_template'),
     'NO_ITEM_SELECTED' => $language->get('admin', 'no_item_selected'),
-    'TEMPLATES' => $templates
+    'TEMPLATES' => $templates,
 ]);
 
 $template->assets()->include([
