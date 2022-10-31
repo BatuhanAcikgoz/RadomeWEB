747787f9461ad1a401afbdd077e1c5840beed7f8
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2022-10-31 16:08:51 +0300

    Update pages.js

--- custom/templates/RadomeWEB/js/core/pages.js
+++ custom/templates/RadomeWEB/js/core/pages.js
@@ -92,9 +92,7 @@ if (page !== '') {
         $(function() {
             var postId = window.location.hash.replace('#post-', '');
             var postElem = '#post-id-' + postId;
-            setTimeout(function() {
-                $('html, body').animate({ scrollTop: $(postElem).offset().top - 15 }, 800);
-            }, 100);
+            setTimeout(function() {}, 100);
         });
 
     } else if (page === 'status') {
