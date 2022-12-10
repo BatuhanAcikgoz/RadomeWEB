f941fb68c214cc82d1b26df20b3cd16fd945dd82
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2022-12-10 13:41:44 +0300

    Update placeholders.php

--- modules/Core/pages/panel/placeholders.php
+++ modules/Core/pages/panel/placeholders.php
@@ -25,27 +25,6 @@ $all_placeholders = Placeholders::getInstance()->getAllPlaceholders();
 
 $template_file = 'integrations/minecraft/placeholders.tpl';
 
-$smarty->assign([
-    'DELETE_PLACEHOLDER' => URL::build('/panel/minecraft/placeholderlar', 'action=delete&id=' . urlencode($placeholder->name))
-]);
-
-if (isset($_GET['action'])) {
-    switch ($_GET['action']) {
-    case 'delete':
-        if (Token::check($_POST['token'])) {
-            if (isset($placeholder->name)) {
-                DB::getInstance()->delete('users_placeholders', ['name', $placeholder->name]);
-                DB::getInstance()->delete('placeholders_settings', ['name', $placeholder->name]);
-
-                Session::flash('admin_mc_servers_success', $language->get('admin', 'server_deleted'));
-            }
-    }
-    
-    default:
-    Redirect::to(URL::build('/panel/minecraft/placeholders'));
-}
-}
-
 if (isset($_GET['leaderboard'])) {
 
     $server_id = $_GET['server_id'];
@@ -187,6 +166,26 @@ if (isset($_GET['leaderboard'])) {
     ]);
 }
 
+$smarty->assign([
+    'DELETE_PLACEHOLDER' => URL::build('/panel/minecraft/placeholderlar', 'action=delete&id=' . urlencode($placeholder->name))
+]);
+
+if (isset($_GET['action'])) {
+    switch ($_GET['action']) {
+    case 'delete':
+        if (Token::check($_POST['token'])) {
+            if (isset($placeholder->name)) {
+                DB::getInstance()->delete('users_placeholders', ['name', $placeholder->name]);
+                DB::getInstance()->delete('placeholders_settings', ['name', $placeholder->name]);
+
+                Session::flash('admin_mc_servers_success', $language->get('admin', 'server_deleted'));
+            }
+    }
+    
+    default:
+    Redirect::to(URL::build('/panel/minecraft/placeholders'));
+}
+}
 
 // Load modules + template
 Module::loadPage($user, $pages, $cache, $smarty, [$navigation, $cc_nav, $staffcp_nav], $widgets, $template);
