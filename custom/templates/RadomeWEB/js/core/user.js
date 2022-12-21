da46a894b5e43a7ea5de098179218761470a91b2
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2022-12-21 23:55:11 +0300

    Update user.js

--- custom/templates/RadomeWEB/js/core/user.js
+++ custom/templates/RadomeWEB/js/core/user.js
@@ -37,11 +37,11 @@ if (loggedIn == 1) {
 						else {
 							if (data.value == 1) {
 								var notification = new Notification(siteName, {
-									body: newMessage1,
+									body: newMessage1, siteIcon,
 								});
 							} else {
 								var notification = new Notification(siteName, {
-									body: x_messages.replace("{{count}}", data.value),
+									body: x_messages.replace("{{count}}", data.value), siteIcon,
 								});
 							}
 
@@ -78,11 +78,11 @@ if (loggedIn == 1) {
 						else {
 							if (data.value == 1) {
 								var notification = new Notification(siteName, {
-									body: newAlert1,
+									body: newAlert1, siteIcon,
 								});
 							} else {
 								var notification = new Notification(siteName, {
-									body: x_alerts.replace("{{count}}", data.value),
+									body: x_alerts.replace("{{count}}", data.value), siteIcon,
 								});
 							}
 
