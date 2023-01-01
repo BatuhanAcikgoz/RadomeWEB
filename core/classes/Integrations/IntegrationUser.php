6e1cef0fef0170ea8914f0405fac3c8f3e9ef564
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2023-01-02 00:54:02 +0300

    Update IntegrationUser.php

--- core/classes/Integrations/IntegrationUser.php
+++ core/classes/Integrations/IntegrationUser.php
@@ -108,12 +108,9 @@ class IntegrationUser {
                 $code
             ]
         );
-        $minecraft_int = DB::getInstance()->get('users_integrations', ['integration_id', $this->_integration->data()->id])->results();
-        if ($minecraft_int == 1) {
             DB::getInstance()->update('users_integrations', $user->data()->id, [
                 'verified' => true
             ]);
-        }
 
         // Load the data for this integration from the query we just made
         $this->_data = new IntegrationUserData($this->_db->query('SELECT * FROM rw_users_integrations WHERE id = ?', [$this->_db->lastId()])->first());
