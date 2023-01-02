1e0c81614bfec4a0fc3ff4a069dda673f5ad111c
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2023-01-02 23:25:08 +0300

    Update new_topic.php

--- modules/Haberler/pages/haberler/new_topic.php
+++ modules/Haberler/pages/haberler/new_topic.php
@@ -23,6 +23,9 @@ $haberler = new Haberler();
 
 // Get user group ID
 $user_groups = $user->getAllGroupIds();
+if ($user->hasPermission('admincp.haberlers')) {
+    Redirect::to(URL::build('/haberler/goruntule/' . urlencode($fid)));
+}
 
 $current_haberler = DB::getInstance()->query('SELECT * FROM rw_haberlers WHERE id = ?', [$fid])->first();
 $haberler_title = Output::getClean($current_haberler->haberler_title);
