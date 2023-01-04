3401875aca746545884112fda2d4358fb4f7929f
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2023-01-04 23:08:06 +0300

    Update view_haberler.php

--- modules/Haberler/pages/haberler/view_haberler.php
+++ modules/Haberler/pages/haberler/view_haberler.php
@@ -353,6 +353,7 @@ if ($haberler_query->redirect_haberler == 1) {
         $results = $paginator->getLimited($topics, 10, $p, count($topics));
         $pagination = $paginator->generate(7, URL::build('/haberler/goruntule/' . urlencode($fid) . '-' . $haberler->titleToURL($haberler_query->haberler_title)));
 
+        $topics = DB::getInstance()->query('SELECT * FROM rw_haberlers WHERE deleted = 0 ORDER BY created DESC')->results();
         if (count($topics)) {
             $smarty->assign('PAGINATION', $pagination);
         } else {
