ddb5b28d56a5e198bde8248fd7d8430aad7bad85
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2023-01-10 00:04:19 +0300

    Update search.php

--- modules/Haberler/pages/haberler/search.php
+++ modules/Haberler/pages/haberler/search.php
@@ -71,7 +71,7 @@ if (!isset($_GET['s'])) {
         foreach ($search_results as $result) {
                         $post = DB::getInstance()->query('SELECT * FROM rw_haberlers WHERE id = ? ORDER BY post_date', [$result->id]);
                         if ($post->count()) {
-                            $post = $post->first();
+                            $post = $post[0];
                             if (!isset($results[$post->id]) && $post->deleted == 0) {
                                 $results[$post->id] = [
                                     'post_id' => $post->id,
