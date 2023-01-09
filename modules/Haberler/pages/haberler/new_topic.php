7f6bc0f0c46062a42a5b83e3e390d51684ee82d8
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2023-01-09 21:37:43 +0300

    Update new_topic.php

--- modules/Haberler/pages/haberler/new_topic.php
+++ modules/Haberler/pages/haberler/new_topic.php
@@ -187,6 +187,7 @@ $id = DB::getInstance()->lastId();
 $users_following = DB::getInstance()->get('users', ['active', 1])->results();
 $topic = DB::getInstance()->get('haberlers', ['id', $id])->results();
 $topic = $topic[0];
+$content = Input::get('content');
 if (count($users_following)) {
     $users_following_info = [];
     foreach ($users_following as $user_following) {
@@ -207,7 +208,7 @@ if (count($users_following)) {
             Output::getClean(SITE_NAME),
             $language->get('emails', 'new_haber', ['author' => $user->data()->username, 'topic' => $topic->haber_title]),
             $language->get('emails', 'greeting'),
-            $language->get('emails', 'new_haber_content', ['author' => $user->data()->username, 'content' => html_entity_decode($topic->post_content)]),
+            $language->get('emails', 'new_haber_content', ['author' => $user->data()->username, 'content' => html_entity_decode($content)]),
             rtrim(URL::getSelfURL(), '/') . URL::build('/haberler/konu/' . urlencode($id) . '-' . $haberler->titleToURL($topic->haber_title)),
             $language->get('emails', 'thanks')
         ],
