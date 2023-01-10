f308e394a5d7cb726b4033fc08ceef2e32d1b624
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2023-01-10 22:09:26 +0300

    Revert "Update search.php"
    This reverts commit 31dfbc3b78e1e657e3fae466014644f4c26cbf34.

--- modules/Haberler/pages/haberler/search.php
+++ modules/Haberler/pages/haberler/search.php
@@ -69,9 +69,8 @@ if (!isset($_GET['s'])) {
 
         $results = [];
         foreach ($search_results as $result) {
-                        $post = DB::getInstance()->query('SELECT * FROM rw_haberlers WHERE id = ? ORDER BY post_date', [$result->id]);
-                        if ($post->count()) {
-                            $post = $post->first();
+                        $post = DB::getInstance()->query('SELECT * FROM rw_haberlers WHERE id = ? ORDER BY post_date', [$result->id])->results();
+                            $post = $post[0];
                             if (!isset($results[$post->id]) && $post->deleted == 0) {
                                 $results[$post->id] = [
                                     'post_id' => $post->id,
@@ -84,11 +83,6 @@ if (!isset($_GET['s'])) {
 
                                 break;
                             }
-
-                            break;
-                        } else {
-                            break;
-                        }
         }
 
         $results = array_values($results);
