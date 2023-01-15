9486b6f2dcf08c11767edab577a1a75fa4530d30
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2023-01-16 02:41:15 +0300

    Update init.php

--- core/init.php
+++ core/init.php
@@ -583,10 +583,9 @@ if ($page != 'install') {
         if (isset($forced) && $forced) {
             // Do they have TFA configured?
             if (!$user->data()->tfa_enabled && rtrim($_GET['route'], '/') != '/cikis') {
-                if (!str_contains($_SERVER['REQUEST_URI'], 'do=enable_tfa')) {
+                if (strpos($_SERVER['REQUEST_URI'], 'do=enable_tfa') === false) {
                     Session::put('force_tfa_alert', $language->get('admin', 'force_tfa_alert'));
                     Redirect::to(URL::build('/kullanici/ayarlar', 'do=enable_tfa'));
-                    exit(0);
                 }
             }
         }
