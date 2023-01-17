0602b5afb4c9944397350a9e124b7bd7da79c9ad
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2023-01-17 14:14:33 +0300

    Update vote.php

--- modules/Vote/pages/vote.php
+++ modules/Vote/pages/vote.php
@@ -35,17 +35,6 @@ foreach ($top_voters as $top_voters) {
 	];
 }
 
-$minecraftmp_votes = 'https://minecraft-mp.com/api/?object=servers&element=votes&key='.$mcmp_key.'&format=json';
-$mcmp_vote2 = json_decode(file_get_contents($minecraftmp_votes));
-$votes_mcmp = $mcmp_vote2->votes;
-$timeago = new TimeAgo(TIMEZONE);
-$votes_array = [];
-foreach ($votes_mcmp as $mcmp_votes) {
-	$votes_array[] = [
-		'nickname' => Output::getClean($mcmp_votes->nickname),
-		'date_friendly' => $timeago->inWords($mcmp_votes->timestamp, $language),
-	];
-}
 
 
 
