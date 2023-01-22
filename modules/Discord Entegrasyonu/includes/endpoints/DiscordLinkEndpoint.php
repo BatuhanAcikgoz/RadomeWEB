5b1c2315781e2e384801581066990954c2611659
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2023-01-22 12:34:03 +0300

    Update DiscordLinkEndpoint.php

--- modules/Discord Entegrasyonu/includes/endpoints/DiscordLinkEndpoint.php
+++ modules/Discord Entegrasyonu/includes/endpoints/DiscordLinkEndpoint.php
@@ -1,5 +1,7 @@
 <?php
 
+use function GuzzleHttp\Promise\queue;
+
 /**
  * @param string $url New Discord bot URL
  * @param string $id New Discord Guild/server ID
@@ -19,6 +21,13 @@ class DiscordLinkEndpoint extends KeyAuthEndpoint {
         if (isset($_POST['player_name']) && isset($_POST['discord_identifier']) && isset($_POST['discord_username'])) {
             $user = DB::getInstance()->get('users', ['username', '=', $_POST['player_name']])->results();
             $user_id = $user[0]->id;
+            $user_discord_integration = DB::getInstance()->query('SELECT * FROM `rw_users_integrations` WHERE integration_id = 2 AND user_id = ?', [$user_id]);
+            $user_discord_integration_id = $user_discord_integration[0]->id;
+            $discord_username = $_POST['discord_username'];
+            $discord_identifier = $_POST['discord_identifier']; 
+
+        if ($user_discord_integration_id === null) {
+            
             
             DB::getInstance()->insert('users_integrations', [
                 'user_id' => $user_id,
@@ -28,6 +37,10 @@ class DiscordLinkEndpoint extends KeyAuthEndpoint {
                 'verified' => 1,
                 'date' => date('U')
             ]);
+        } else {
+            
+            DB::getInstance()->query("UPDATE rw_users_integrations SET identifier = {$discord_identifier}, username = {$discord_username} WHERE user_id = {$user_id} AND integration_id = 2");
+        }
 
             $api->returnArray(['message' => Discord::getLanguageTerm('discord_settings_updated')]);
             }
