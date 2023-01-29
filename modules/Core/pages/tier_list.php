3a8055df9a1287661745dc8d846b8e840b05e52d
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2023-01-30 00:11:58 +0300

    Update tier_list.php

--- modules/Core/pages/tier_list.php
+++ modules/Core/pages/tier_list.php
@@ -43,7 +43,8 @@ foreach ($tier_list_db as $leaderboard_placeholder) {
     // TODO: move this to placeholders class
     foreach ($data as $rowlt1) {
         $row_data = new stdClass();
-        $row_data->style = $rowlt1->username->getGroupStyle();
+        $user = new User($rowlt1->id);
+        $row_data->style = $user->getGroupStyle();
         $row_data->username = Output::getClean($rowlt1->username);
         $row_data->avatar = AvatarSource::getAvatarFromUUID($rowlt1->username, 24);
 
