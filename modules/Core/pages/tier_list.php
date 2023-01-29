e8520cc82a558978f71cd99b3a72c7970907b3b7
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2023-01-29 19:49:09 +0300

    Update tier_list.php

--- modules/Core/pages/tier_list.php
+++ modules/Core/pages/tier_list.php
@@ -44,8 +44,9 @@ foreach ($tier_list_db as $leaderboard_placeholder) {
     foreach ($data as $rowlt1) {
         $row_data = new stdClass();
 
-        $row_data->username = Output::getClean($rowlt1->username->data()->username);
-        $row_data->avatar = AvatarSource::getAvatarFromUUID($username, 24);
+        $username_1 = $rowlt1->username;
+        $row_data->username = Output::getClean($username_1->data()->username);
+        $row_data->avatar = AvatarSource::getAvatarFromUUID($username_1, 24);
 
         $leaderboard_placeholders_data[] = $row_data;
     }
