5eb78655986f86ed07f25f385925a397f0b66bf0
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2023-01-31 21:18:34 +0300

    Update tier_list.php

--- modules/Core/pages/tier_list.php
+++ modules/Core/pages/tier_list.php
@@ -22,7 +22,7 @@ if (Util::getSetting('tier_list_page') !== '1') {
     die();
 }
 
-const PAGE = 'leaderboards';
+const PAGE = 'tier_list';
 $page_title = $language->get('admin', 'tier_list');
 require_once(ROOT_PATH . '/core/templates/frontend_init.php');
 
@@ -57,120 +57,120 @@ foreach ($tier_list_db as $leaderboard_placeholder) {
     // TODO: move this to placeholders class
     foreach ($datalt1 as $rowlt1) {
         $row_datalt1 = new stdClass();
-        $user = new User($rowlt1->id, 'id');
+        $tier_user = new User($rowlt1->id);
         $row_datalt1->user_id = $rowlt1->id;
         $row_datalt1->name = $leaderboard_placeholder->name;
-        $row_datalt1->username = $user->getDisplayname(true);
-        $row_datalt1->avatar = $user->getAvatar(32);
-        $row_datalt1->profile_url = $user->getProfileURL();
+        $row_datalt1->username = $tier_user->getDisplayname(true);
+        $row_datalt1->avatar = $tier_user->getAvatar(32);
+        $row_datalt1->profile_url = $tier_user->getProfileURL();
 
         $tier_list_lt1_data[] = $row_datalt1;
     }
 
     foreach ($datalt2 as $rowlt2) {
         $row_datalt2 = new stdClass();
-        $user = new User($rowlt2->id, 'id');
+        $tier_user = new User($rowlt2->id);
         $row_datalt2->user_id = $rowlt2->id;
         $row_datalt2->name = $leaderboard_placeholder->name;
-        $row_datalt2->username = $user->getDisplayname(true);
-        $row_datalt2->avatar = $user->getAvatar(32);
-        $row_datalt2->profile_url = $user->getProfileURL();
+        $row_datalt2->username = $tier_user->getDisplayname(true);
+        $row_datalt2->avatar = $tier_user->getAvatar(32);
+        $row_datalt2->profile_url = $tier_user->getProfileURL();
 
         $tier_list_lt2_data[] = $row_datalt2;
     }
 
     foreach ($datalt3 as $rowlt3) {
         $row_datalt3 = new stdClass();
-        $user = new User($rowlt3->id, 'id');
+        $tier_user = new User($rowlt3->id);
         $row_datalt3->user_id = $rowlt3->id;
         $row_datalt3->name = $leaderboard_placeholder->name;
-        $row_datalt3->username = $user->getDisplayname(true);
-        $row_datalt3->avatar = $user->getAvatar(32);
-        $row_datalt3->profile_url = $user->getProfileURL();
+        $row_datalt3->username = $tier_user->getDisplayname(true);
+        $row_datalt3->avatar = $tier_user->getAvatar(32);
+        $row_datalt3->profile_url = $tier_user->getProfileURL();
 
         $tier_list_lt3_data[] = $row_datalt3;
     }
 
     foreach ($datalt4 as $rowlt4) {
         $row_datalt4 = new stdClass();
-        $user = new User($rowlt4->id, 'id');
+        $tier_user = new User($rowlt4->id);
         $row_datalt4->user_id = $rowlt4->id;
         $row_datalt4->name = $leaderboard_placeholder->name;
-        $row_datalt4->username = $user->getDisplayname(true);
-        $row_datalt4->avatar = $user->getAvatar(32);
-        $row_datalt4->profile_url = $user->getProfileURL();
+        $row_datalt4->username = $tier_user->getDisplayname(true);
+        $row_datalt4->avatar = $tier_user->getAvatar(32);
+        $row_datalt4->profile_url = $tier_user->getProfileURL();
 
         $tier_list_lt4_data[] = $row_datalt4;
     }
 
     foreach ($datalt5 as $rowlt5) {
         $row_datalt5 = new stdClass();
-        $user = new User($rowlt5->id, 'id');
+        $tier_user = new User($rowlt5->id);
         $row_datalt5->user_id = $rowlt5->id;
         $row_datalt5->name = $leaderboard_placeholder->name;
-        $row_datalt5->username = $user->getDisplayname(true);
-        $row_datalt5->avatar = $user->getAvatar(32);
-        $row_datalt5->profile_url = $user->getProfileURL();
+        $row_datalt5->username = $tier_user->getDisplayname(true);
+        $row_datalt5->avatar = $tier_user->getAvatar(32);
+        $row_datalt5->profile_url = $tier_user->getProfileURL();
 
         $tier_list_lt5_data[] = $row_datalt5;
     }
     
     foreach ($dataht1 as $rowht1) {
         $row_dataht1 = new stdClass();
-        $user = new User($rowht1->id, 'id');
+        $tier_user = new User($rowht1->id);
         $row_dataht1->user_id = $rowht1->id;
         $row_dataht1->name = $leaderboard_placeholder->name;
-        $row_dataht1->username = $user->getDisplayname(true);
-        $row_dataht1->avatar = $user->getAvatar(32);
-        $row_dataht1->profile_url = $user->getProfileURL();
+        $row_dataht1->username = $tier_user->getDisplayname(true);
+        $row_dataht1->avatar = $tier_user->getAvatar(32);
+        $row_dataht1->profile_url = $tier_user->getProfileURL();
 
         $tier_list_ht1_data[] = $row_dataht1;
     }
 
     foreach ($dataht2 as $rowht2) {
         $row_dataht2 = new stdClass();
-        $user = new User($rowht2->id, 'id');
+        $tier_user = new User($rowht2->id);
         $row_dataht2->user_id = $rowht2->id;
         $row_dataht2->name = $leaderboard_placeholder->name;
-        $row_dataht2->username = $user->getDisplayname(true);
-        $row_dataht2->avatar = $user->getAvatar(32);
-        $row_dataht2->profile_url = $user->getProfileURL();
+        $row_dataht2->username = $tier_user->getDisplayname(true);
+        $row_dataht2->avatar = $tier_user->getAvatar(32);
+        $row_dataht2->profile_url = $tier_user->getProfileURL();
 
         $tier_list_ht2_data[] = $row_dataht2;
     }
 
     foreach ($dataht3 as $rowht3) {
         $row_dataht3 = new stdClass();
-        $user = new User($rowht3->id, 'id');
+        $tier_user = new User($rowht3->id);
         $row_dataht3->user_id = $rowht3->id;
         $row_dataht3->name = $leaderboard_placeholder->name;
-        $row_dataht3->username = $user->getDisplayname(true);
-        $row_dataht3->avatar = $user->getAvatar(32);
-        $row_dataht3->profile_url = $user->getProfileURL();
+        $row_dataht3->username = $tier_user->getDisplayname(true);
+        $row_dataht3->avatar = $tier_user->getAvatar(32);
+        $row_dataht3->profile_url = $tier_user->getProfileURL();
 
         $tier_list_ht3_data[] = $row_dataht3;
     }
 
     foreach ($dataht4 as $rowht4) {
         $row_dataht4 = new stdClass();
-        $user = new User($rowht4->id, 'id');
+        $tier_user = new User($rowht4->id);
         $row_dataht4->user_id = $rowht4->id;
         $row_dataht4->name = $leaderboard_placeholder->name;
-        $row_dataht4->username = $user->getDisplayname(true);
-        $row_dataht4->avatar = $user->getAvatar(32);
-        $row_dataht4->profile_url = $user->getProfileURL();
+        $row_dataht4->username = $tier_user->getDisplayname(true);
+        $row_dataht4->avatar = $tier_user->getAvatar(32);
+        $row_dataht4->profile_url = $tier_user->getProfileURL();
 
         $tier_list_ht4_data[] = $row_dataht4;
     }
 
     foreach ($dataht5 as $rowht5) {
         $row_dataht5 = new stdClass();
-        $user = new User($rowht5->id, 'id');
+        $tier_user = new User($rowht5->id);
         $row_dataht5->user_id = $rowht5->id;
         $row_dataht5->name = $leaderboard_placeholder->name;
-        $row_dataht5->username = $user->getDisplayname(true);
-        $row_dataht5->avatar = $user->getAvatar(32);
-        $row_dataht5->profile_url = $user->getProfileURL();
+        $row_dataht5->username = $tier_user->getDisplayname(true);
+        $row_dataht5->avatar = $tier_user->getAvatar(32);
+        $row_dataht5->profile_url = $tier_user->getProfileURL();
 
         $tier_list_ht5_data[] = $row_dataht5;
     }
@@ -228,7 +228,7 @@ $template->addJSScript('
 ');
 
 // Load modules + template
-Module::loadPage($user, $pages, $cache, $smarty, [$navigation, $cc_nav, $staffcp_nav], $widgets, $template);
+Module::loadPage($tier_user, $pages, $cache, $smarty, [$navigation, $cc_nav, $staffcp_nav], $widgets, $template);
 
 $template->onPageLoad();
 
