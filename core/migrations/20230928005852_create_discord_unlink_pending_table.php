0fff1801f2c553bf34cf2c86689ef4026df00ea9
BatuhanAcikgoz <batuhanacikgoz04@gmail.com>
2023-03-02 02:12:29 +0300

    misc fix

--- core/migrations/20230928005852_create_discord_unlink_pending_table.php
+++ core/migrations/20230928005852_create_discord_unlink_pending_table.php
@@ -13,7 +13,7 @@ final class CreateDiscordUnlinkPendingTable extends AbstractMigration
             ->addColumn('command', 'string', ['length' => 2048])
             ->addColumn('date', 'integer', ['length' => 11])
             ->addColumn('status', 'smallinteger', ['length' => 1, 'null' => false, 'default' => 0])
-            ->addColumn('executed_date', 'integer', ['length' => 11]);
+            ->addColumn('executed_date', 'integer', ['length' => 11, 'null' => true]);
 
         $table->create();
     }
