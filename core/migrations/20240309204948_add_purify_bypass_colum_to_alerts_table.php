e14074aa5441c303a776e317bf83d1776b5421a7
Batuhan Açıkgöz <batuhanacikgoz04@gmail.com>
2024-06-16 19:53:15 +0300

    Update

--- core/migrations/20240309204948_add_purify_bypass_colum_to_alerts_table.php
+++ core/migrations/20240309204948_add_purify_bypass_colum_to_alerts_table.php
@@ -10,6 +10,6 @@ final class AddPurifyBypassColumToAlertsTable extends AbstractMigration
     {
         $this->table('rw_alerts')
             ->addColumn('bypass_purify', 'boolean', ['default' => false])
-            ->update();$page_title = $language->get('user', 'user_cp');
+            ->update();
     }
 }
